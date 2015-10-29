05f07db63f68a72e06e425bc3997df8f2d3fe1f6
wassim ajjali <wassim.ajjali@he-arc.ch>
2015-10-29 17:22:52 +0100

    navigation à partir du menu principal

--- Project/app/src/main/java/hearc/othello/InstructionActivity.java
+++ Project/app/src/main/java/hearc/othello/InstructionActivity.java
@@ -1,30 +1,30 @@
 package hearc.othello;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class HomeActivity extends AppCompatActivity implements Button.OnClickListener{
+public class InstructionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_home);
+        setContentView(R.layout.layout_instruction);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_instruction, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -33,18 +33,5 @@ public class HomeActivity extends AppCompatActivity implements Button.OnClickLis
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.instruction:
-            Intent intent = new Intent(HomeActivity.this, InstructionActivity.class);
-            startActivity(intent);
-                
-
-        }
-
-
     }
 }
